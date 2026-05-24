b47e5014514434d011824de50906c87c930bdf30
yigityanik <yeyanik123@gmail.com>
2026-05-24 05:14:02 +0300

    GM9PRO_sprout: add interaction boost support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,6 +91,9 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
+# Power
+TARGET_USES_INTERACTION_BOOST := true
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
